8f31991405ab5c6fffb45fdbdfabf218ec6e0b06
Kyrylo1122 <93613807+Kyrylo1122@users.noreply.github.com>
2022-10-24 13:21:56 +0200

    add

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/films-react/main/">
+    <BrowserRouter basename="/films-react/">
       <Provider store={store}>
         <PersistGate persistor={persist}>
           <App />
